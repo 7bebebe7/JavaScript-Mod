d80913e3bbcfbe0ba681faf979e3175894c1e9ae
7bebebe7 <oleh.sobchenko@gmail.com>
2026-06-10 21:45:39 +0300

    button test

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,18 @@ require("turrets")
 require("particles")
 require("bullets")
 require("payload")
+
+Events.on(ClientLoadEvent, e => {
+    const button = new TextButton("Log");
+
+    button.clicked(() => {
+        Log.info("be");
+    });
+
+    button.visible(() => Vars.state.isGame());
+
+    Vars.ui.hudGroup.fill(t => {
+        t.bottom().left();
+        t.add(button).size(150, 60).pad(10);
+    });
+});
