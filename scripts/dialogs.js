efc38e538f8d936e8dac5cc7d5dacd589f03025a
7bebebe7 <oleh.sobchenko@gmail.com>
2026-06-14 18:06:17 +0300

    DM Item Menu 12 (DM/IM)

--- scripts/dialogs.js
+++ scripts/dialogs.js
@@ -141,9 +141,7 @@ function DevMenu() {
 function Resources() {
     const res = new BaseDialog("Resources");
     
-    let label = res.cont.add("0").center().get()
-    
-    res.cont.row();
+    res.cont.add("0").center().get().row()
     
     res.cont.button(Icon.cancel,  () => { }).size(50,50).padRight(5)
     
