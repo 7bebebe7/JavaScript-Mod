f02e34c4053c95fa2afb4a24cadef0da1843ea8b
7bebebe7 <oleh.sobchenko@gmail.com>
2026-06-28 15:50:15 +0300

    new block test "electric-generator"

---
+++ scripts/power.js
@@ -0,0 +1,23 @@
+//import-----
+
+//code-----
+const PEGS = new Sound("Eplace")
+
+
+const EG = extend(PowerSource, "electric-generator", {
+  size: 3,
+  health: 1350,
+  category: Category.power,
+  buildVisibility: BuildVisibility.shown,
+  requirements: ItemStack.with(
+    Items.thorium, 200,
+    Items.titanium, 100,
+    Items.phaseFabric, 150,
+    Items.graphite, 120
+  ),
+  update: true,
+  powerProduction: 50,
+  placeSound: PEGS
+});
+
+//othere-----
