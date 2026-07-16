30808ea510593810c4cca18f5bbb2d1cf4a46729
7bebebe7 <oleh.sobchenko@gmail.com>
2026-07-16 14:32:05 +0300

    moo fix 7

--- scripts/units.js
+++ scripts/units.js
@@ -9,7 +9,7 @@ const moo = extend(UnitType, "mushroom", {
     mechFrontSway: 0.3,
     mechStepShake: 0.4,
     buildSpeed: 3,
-    hitsize: 8,
+    hitSize: 8,
     legCount: 2,
     legGroupSize: 1,
 });
