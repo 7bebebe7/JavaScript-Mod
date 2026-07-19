34612e7416f84c56f701536a5ffb52c74834e18a
7bebebe7 <oleh.sobchenko@gmail.com>
2026-07-19 21:32:56 +0300

    new metod for moo, mario mechanical

--- scripts/units.js
+++ scripts/units.js
@@ -1,9 +1,26 @@
+const marioKillAbility = new Ability(){
+    update: function(unit){
+        if(unit.lastFlying === undefined) unit.lastFlying = false;
+
+        if(unit.lastFlying && !unit.isFlying()){
+            let tile = Vars.world.tileWorld(unit.x, unit.y);
+            let marioBlock = Vars.content.getByName(ContentType.block, "mario");
+
+            if(tile != null && tile.build != null && tile.block() == marioBlock){
+                tile.build.kill();
+            }
+        }
+
+        unit.lastFlying = unit.isFlying();
+    }
+};
+
 const moo = extend(UnitType, "mushroom", {
     health: 7500,
     armor: 9,
     speed: 0.38,
     flying: false,
-    canBoost: true,    
+    canBoost: true,
     boostMultiplier: 3,
     mechFrontSway: 0.3,
     mechStepShake: 0.4,
@@ -11,26 +28,5 @@ const moo = extend(UnitType, "mushroom", {
     hitSize: 8,
     legCount: 2,
     legGroupSize: 1,
-    
-    constructor: new Prov(function(){
-        return extend(MechUnit, {
-            lastFlying: false,
-    
-            update: function(){
-                this.super$update();
-    
-                if(this.lastFlying && !this.isFlying()){
-    
-                    let tile = Vars.world.tileWorld(this.x, this.y);
-                    let marioBlock = Vars.content.getByName(ContentType.block, "mario");
-    
-                    if(tile != null && tile.build != null && tile.block() == marioBlock){
-                        tile.build.kill();
-                    }
-                }
-    
-                this.lastFlying = this.isFlying();
-            }
-        });
-    })
+    abilities: [marioKillAbility]
 });
