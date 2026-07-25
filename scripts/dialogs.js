ab65a07bacf0a9831a64a9b9a362912c2056c00b
7bebebe7 <oleh.sobchenko@gmail.com>
2026-07-25 19:23:55 +0300

    update dialogs.js

--- scripts/dialogs.js
+++ scripts/dialogs.js
@@ -168,8 +168,8 @@ Events.on(ClientLoadEvent, e => {
         }).size(200,200).padLeft(30).padTop(10).row();
         
         upm.cont.table(Tex.button, t => {
-            t.label(() => "12345").fillX()
-        }).colspan(3)
+            t.add("12345").center();
+        }).fillX().colspan(3);
         
         upm.show()
     }
